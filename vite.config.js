4d7bf0dacd85648aa9b890ed9ead90dcf9635943
Lawrence Pinto <lpinto1204@gmail.com>
2025-03-19 17:09:46 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    headers: {
+      "Content-Type": "text/javascript; charset=utf-8",
+    },
+  },
   plugins: [react(), tailwindcss()],
   base: "/lawrence-portfolio",
 });
